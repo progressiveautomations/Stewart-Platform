dcaf6dee5d1e60088a8f5f01745982515cf9a916
Henry Liu <henryymliu@gmail.com>
2018-03-29 13:59:47 -0700

    Fixed misusing arcsin/cos functions

--- GUI/leap_event_listener.cpp
+++ GUI/leap_event_listener.cpp
@@ -25,27 +25,22 @@ void LeapEventListener::onFrame(const Leap::Controller &controller)
             qreal yaw = hand.direction().yaw();
             qreal roll = hand.palmNormal().roll();
 
-            qreal cos_roll = qAcos(roll);
-            qreal cos_pitch = qAcos(pitch);
-            qreal cos_yaw = qAcos(yaw);
-            qreal sin_roll = qAsin(roll);
-            qreal sin_pitch = qAsin(pitch);
-            qreal sin_yaw = qAsin(yaw);
+            qreal cos_roll = qCos(roll);
+            qreal cos_pitch = qCos(pitch);
+            qreal cos_yaw = qCos(yaw);
+            qreal sin_roll = qSin(roll);
+            qreal sin_pitch = qSin(pitch);
+            qreal sin_yaw = qSin(yaw);
 
             QMatrix4x4 transform_matrix(cos_yaw * cos_pitch, cos_pitch * sin_yaw, -sin_pitch, pos.x,
                                         cos_yaw * sin_pitch * sin_roll - sin_yaw * cos_roll, cos_yaw * cos_roll + sin_roll * sin_yaw * sin_pitch, cos_pitch * sin_roll, -pos.z,
                                         cos_yaw * sin_pitch * cos_roll + sin_yaw * sin_roll, -cos_yaw * sin_roll + cos_roll * sin_yaw * sin_pitch, cos_pitch * cos_roll, pos.y + HOME_POSITION_HEIGHT,
                                         0, 0, 0, 1);
 
-//            QMatrix4x4 transform_matrix(cos_yaw * cos_pitch, cos_yaw * sin_pitch * sin_roll - sin_yaw * cos_roll, cos_yaw * sin_pitch * cos_roll + sin_yaw * sin_roll, 0,
-//                                        sin_yaw * cos_pitch, sin_yaw * sin_pitch * sin_roll + cos_yaw * cos_roll, sin_yaw * sin_pitch * cos_roll - cos_yaw * sin_roll, 0,
-//                                        -sin_pitch, cos_pitch * sin_roll, cos_pitch * cos_roll, 0,
-//                                        0, 0, 0, 1);
-
             QVector<int> actuator_lengths(StewartPlatform::NUM_ACTUATORS, 0);
             for (int i = 0; i < StewartPlatform::NUM_ACTUATORS; ++i)
             {
-                QVector4D eff_pos = transform_matrix * END_EFF_POS[i]; // + QVector4D(pos.x, -pos.z, pos.y + HOME_POSITION_HEIGHT, 0.0f);
+                QVector4D eff_pos = transform_matrix * END_EFF_POS[i];
                 actuator_lengths[i] = static_cast<int>((eff_pos.toVector3D() - BASE_POS[i]).length() - MIN_ACTUATOR_LEN);
             }
             emit LeapFrameUpdate(actuator_lengths);
